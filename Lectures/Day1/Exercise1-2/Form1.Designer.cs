abb97f1218004f2a0eb6f0804c51faeebc3dff3a
natebakescakes <natebakescakes@gmail.com>
2018-03-16 13:55:45 +0800

    Complete Exercise 1.2

---
+++ Lectures/Day1/Exercise1-2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Lectures.Day1.Exercise1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(672, 562);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 634);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Movie Rating";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(348, 631);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 38);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 697);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 68);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Retrieve";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 777);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lectures/Day1/Exercise1-2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lectures.Day1.Exercise1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputMovieRating = textBox1.Text;
+
+            string connectionString =
+                "data source=(local);" +
+                "integrated security=SSPI;" +
+                "initial catalog=Dafesty";
+
+            var connection = new SqlConnection(connectionString);
+            var command = new SqlCommand();
+
+            command.Connection = connection;
+            if (inputMovieRating != "")
+            {
+                command.CommandText =
+                    $"SELECT MovieTitle FROM Movies " +
+                    $"WHERE Rating = '{inputMovieRating}' " +
+                    $"ORDER BY MovieTitle ASC";
+            }
+            else
+            {
+                command.CommandText =
+                    "SELECT MovieTitle FROM Movies " +
+                    "ORDER BY MovieTitle ASC";
+            }
+
+            connection.Open();
+
+            var reader = command.ExecuteReader();
+
+            listBox1.Items.Clear();
+
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader["MovieTitle"].ToString());
+            }
+
+            connection.Close();
+            
+        }
+    }
+}
