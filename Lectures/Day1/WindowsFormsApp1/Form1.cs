0fb1cddf4f59c3601d8dfb2def7ae4e1db33bb53
natebakescakes <natebakescakes@gmail.com>
2018-03-16 10:40:51 +0800

    Add first lecture example

---
+++ Lectures/Day1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lectures.Day1.WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Setup connectionString default values
+            string connectionString = 
+                "data source=(local); " +
+                "integrated security=SSPI; " +
+                "initial catalog=Dafesty; ";
+
+            // Instantiate SqlConnection and SqlCommand objects
+            var connection = new SqlConnection(connectionString);
+            var command = new SqlCommand();
+
+            // Set SQL Query to .CommandText
+            command.CommandText = 
+                "UPDATE Customers SET MemberCategory = 'D' " +
+                "WHERE CustomerId = '1000'";
+
+            // Link command to the connection
+            command.Connection = connection;
+
+            // Execute Non-query for DDL statements
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# gdipsa-esnet
-GDipSA: Enterprise System Development under .NET Framework
+# gdipsa-.net
+## GDipSA: Enterprise System Development under .NET Framework
+
+This repository contains workshop solutions for each day of the Enterprise System Development under .NET Framework module (ESNET) for the Graduate Diploma in Systems Analysis.
+
+---
+
+## Folder Structure
+
+- esnet (Enterprise System Development under .NET Framework)
+    - Lectures (Contains examples done during class)
+    - Workshops (Contains SQL files for each workshop)
