b47a8dbe8288fcfcd330f70599e215a3746ff102
natebakescakes <natebakescakes@gmail.com>
2018-03-19 14:11:00 +0800

    Complete Lunch Exercise

---
+++ Lectures/Day2/LunchExercise/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Lectures.Day2.LunchExercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.QueryText = new System.Windows.Forms.TextBox();
+            this.MovieTitleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 40;
+            this.dataGridView1.Size = new System.Drawing.Size(1631, 856);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(13, 13);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(228, 38);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Load Data";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(247, 13);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(280, 38);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Query Video Code";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // QueryText
+            // 
+            this.QueryText.Location = new System.Drawing.Point(534, 13);
+            this.QueryText.Name = "QueryText";
+            this.QueryText.Size = new System.Drawing.Size(143, 38);
+            this.QueryText.TabIndex = 4;
+            // 
+            // MovieTitleLabel
+            // 
+            this.MovieTitleLabel.AutoSize = true;
+            this.MovieTitleLabel.Location = new System.Drawing.Point(684, 13);
+            this.MovieTitleLabel.Name = "MovieTitleLabel";
+            this.MovieTitleLabel.Size = new System.Drawing.Size(0, 32);
+            this.MovieTitleLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1631, 913);
+            this.Controls.Add(this.MovieTitleLabel);
+            this.Controls.Add(this.QueryText);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.TextBox QueryText;
+        private System.Windows.Forms.Label MovieTitleLabel;
+    }
+}
+
---
+++ Lectures/Day2/LunchExercise/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lectures.Day2.LunchExercise
+{
+    public partial class Form1 : Form
+    {
+        DafestyEntities context;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            context = new DafestyEntities();
+
+            dataGridView1.DataSource = context.Movies.ToList();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (QueryText.Text != "")
+            {
+                MovieTitleLabel.Text = context.Movies.Where(x => x.VideoCode.ToString() == QueryText.Text).First().MovieTitle;
+            }
+            else
+            {
+                MessageBox.Show("Query Text is blank.");
+            }
+        }
+    }
+}
