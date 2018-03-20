8c06838a7cc33893e7416933cbbe3e9af4a37a98
natebakescakes <natebakescakes@gmail.com>
2018-03-20 10:14:56 +0800

    Complete single entity lecture

---
+++ Lectures/Day3/EFDemoContinued/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFDemoContinued
+{
+    public partial class Form1 : Form
+    {
+        DafestyEntities context;
+
+        public Form1()
+        {
+            InitializeComponent();
+            context = new DafestyEntities();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = (from x in context.Customers
+                                        select new { x.CustomerID, x.CustomerName, x.MemberRating })
+                                        .ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //var car1 = new Car("Toyota", "Corolla");
+            //var car2 = new Car("Honda", "City");
+            //var car3 = new Car("Toyota", "Camry");
+
+            //var list = new List<Car>() { car1, car2, car3 };
+
+            var list = new List<Car>() {
+                new Car("Toyota", "Corolla"),
+                new Car("Honda", "City"),
+                new Car("Toyota", "Camry")
+            };
+
+            label1.Text = list[1].Model;
+        }
+    }
+
+    class Car
+    {
+        public Car(string make, string model)
+        {
+            this.Model = model;
+            this.Make = make;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
---
+++ Lectures/Day3/EFDemoContinued/Form2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFDemoContinued
+{
+    public partial class Form2 : Form
+    {
+        DafestyEntities context;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            context = new DafestyEntities();
+
+            // Get existing Customer
+            Customer c = context.Customers.First(x => x.CustomerID == "1001");
+            label1.Text = $"{c.CustomerName} {c.MemberRating}";
+
+            // Change customer's member rating
+            c.MemberRating = "B";
+            label1.Text = $"{c.CustomerName} {c.MemberRating}";
+
+            // Create new customer
+            var newCustomer = new Customer();
+            newCustomer.CustomerID = "1007";
+            newCustomer.CustomerName = "Bond";
+            newCustomer.MemberRating = "Z";
+
+            // Add new customer to Customers collection
+            context.Customers.Add(newCustomer);
+
+            // Fetch customer
+            var deleteCustomer = context.Customers.First(x => x.CustomerID == "1007");
+
+            // Remove customer
+            context.Customers.Remove(deleteCustomer);
+
+            // Save Changes to sync DB
+            context.SaveChanges();
+        }
+    }
+}
